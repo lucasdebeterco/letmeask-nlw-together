df3a84ea55590e5189b4bafc3fa42fc2fdfb872c
Lucas Debeterco <lucasdebeterco@gmail.com>
2022-02-23 23:16:57 -0300

    Corrigido problema na criação das rotas com react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import { Home } from './pages/Home'
 import { NewRoom } from './pages/NewRoom'
 
 function App() {
 	return (
-		<div>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/rooms/new" element={<NewRoom />} />
-			</Routes>
-		</div>
+		<Router>
+			<div>
+				<Routes>
+					<Route path="" element={<Home />} />
+					<Route path="/rooms/new" element={<NewRoom />} />
+				</Routes>
+			</div>
+		</Router>
 	);
 }
 
