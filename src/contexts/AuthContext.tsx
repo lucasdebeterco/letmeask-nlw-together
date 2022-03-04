58f0b12ef8878ba231a3a149de08b5613bfa0b4c
Lucas Debeterco <lucasdebeterco@gmail.com>
2022-03-04 00:30:06 -0300

    Correção import

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,5 @@
-import firebase from "firebase/compat";
 import { createContext, ReactNode, useEffect, useState } from "react";
-import { auth } from "../services/firebase";
+import { auth, firebase } from "../services/firebase";
 
 type User = {
 	id: String;
