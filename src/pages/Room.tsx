6f8d2b2cb68f7fc335fd19cbd82d9781ee3dee92
Lucas Debeterco <lucasdebeterco@gmail.com>
2022-04-04 00:10:27 -0300

    Criado componente do código da sala

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,14 +1,23 @@
+import { useParams } from 'react-router-dom';
+
 import logoImg from '../assets/logo.svg'
+import { RoomCode } from '../components/RoomCode';
 import { Button } from '../components/Button';
 import '../styles/room.scss';
 
+type RoomParams = {
+    id: string;
+}
+
 export function Room() {
+    const params = useParams<RoomParams>();
+
     return (
         <div id="page-room">
             <header>
                 <div className="content">
                     <img src={logoImg} alt="Letmeask" />
-                    <div>Codigo</div>
+                    <RoomCode code={params.id!} />
                 </div>
             </header>
 
