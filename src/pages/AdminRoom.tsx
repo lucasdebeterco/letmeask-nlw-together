8d6a8ba64a2880220de785a9d0891deba151c769
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2022-07-10 23:36:03 -0300

    Implementando remoção de perguntas pelo admin

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import { database, firebase } from '../services/firebase';
 
 import logoImg from '../assets/logo.svg'
+import deleteImg from '../assets/delete.svg'
 import '../styles/room.scss';
 
 import { Question } from '../components/Question';
@@ -21,6 +22,12 @@ export function AdminRoom() {
     const params = useParams<RoomParams>();
     const roomId = params.id!;
     const { title, questions} = useRoom(roomId)
+
+    async function handleDeleteQuestion(questionId: string) {
+        if(window.confirm('Você tem certeza que deseja excluir essa pergunta?')) {
+            const questionRed = await firebase.database().ref(`rooms/${roomId}/questions/${questionId}`).remove();
+        }
+    }
 
     return (
         <div id="page-room">
@@ -47,7 +54,13 @@ export function AdminRoom() {
                                 key={question.id}
                                 content={question.content}
                                 author={question.author}
-                            />
+                            >
+                                <button
+                                type='button'
+                                onClick={() => handleDeleteQuestion(question.id)}>
+                                    <img src={deleteImg} alt="Remover pergunta" />
+                                </button>
+                            </Question>
                         );
                     })}
                 </div>
