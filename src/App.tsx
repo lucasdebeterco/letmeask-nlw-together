ed0aa30c6f5e1c185a6e13f5fe737834f9fad34c
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2022-07-10 21:10:40 -0300

    Criada página do admin

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { NewRoom } from './pages/NewRoom'
 import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
 import { AuthContextProvider } from './contexts/AuthContext'
 
@@ -16,7 +17,8 @@ function App() {
 				<Routes>
 					<Route path="" element={<Home />} />
 					<Route path="/rooms/new" element={<NewRoom />} />
-					<Route path="/rooms/:id" element={<Room />} />	
+					<Route path="/rooms/:id" element={<Room />} />
+					<Route path="/admin/rooms/:id" element={<AdminRoom />} />	
 				</Routes>
 			</AuthContextProvider>
 			</div>
